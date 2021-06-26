1bfdf2d8793101dc8997c1eeccfc6237406b8d8d
XiuJian <xiujian@shinetechchina.com>
2021-06-26 08:19:19 +0800

    Add project files.

---
+++ Consumer_Test/Program.cs
@@ -0,0 +1,142 @@
+﻿using Avro.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using io.confluent.ksql.avro_schemas;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace Consumer_Test
+{
+    class Program
+    {
+        private static readonly string topicName = "NONPHARM_CPOY_LIVE";// "NONPHARM_LIVE";// "ca6340e403ac.dbo.Items"; //DEMO_LIVE_1   
+        private static readonly string ipinfo = "172.27.0.189";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "consumers_test_22",
+                BootstrapServers = $"{ipinfo}:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                SecurityProtocol = SecurityProtocol.Plaintext, 
+            };
+
+            var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig
+            {
+                SchemaRegistryUrl = $"http://{ipinfo}:8081"
+            });
+            //SchemaAvroSerDes
+
+
+            //using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+            //    // Note: All handlers are called on the main .Consume thread.
+            //    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+            //    .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+            //    .SetPartitionsAssignedHandler((c, partitions) =>
+            //    {
+            //        // Since a cooperative assignor (CooperativeSticky) has been configured, the
+            //        // partition assignment is incremental (adds partitions to any existing assignment).
+            //        Console.WriteLine(
+            //            "Partitions incrementally assigned: [" +
+            //            string.Join(',', partitions.Select(p => p.Partition.Value)) +
+            //            "], all: [" +
+            //            string.Join(',', c.Assignment.Concat(partitions).Select(p => p.Partition.Value)) +
+            //            "]");
+
+            //        // Possibly manually specify start offsets by returning a list of topic/partition/offsets
+            //        // to assign to, e.g.:
+            //        // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+            //    })
+            //    .SetPartitionsRevokedHandler((c, partitions) =>
+            //    {
+            //        // Since a cooperative assignor (CooperativeSticky) has been configured, the revoked
+            //        // assignment is incremental (may remove only some partitions of the current assignment).
+            //        var remaining = c.Assignment.Where(atp => partitions.Where(rtp => rtp.TopicPartition == atp).Count() == 0);
+            //        Console.WriteLine(
+            //            "Partitions incrementally revoked: [" +
+            //            string.Join(',', partitions.Select(p => p.Partition.Value)) +
+            //            "], remaining: [" +
+            //            string.Join(',', remaining.Select(p => p.Partition.Value)) +
+            //            "]");
+            //    })
+            //    .Build())
+            //{
+            //    consumer.Subscribe(topicName);
+
+            //    try
+            //    {
+            //        while (true)
+            //        {
+            //            try
+            //            {
+            //                var consumeResult = consumer.Consume();
+
+            //                if (consumeResult.IsPartitionEOF)
+            //                {
+            //                    Console.WriteLine(
+            //                        $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+            //                    continue;
+            //                }
+
+            //                Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+
+            //                try
+            //                {
+            //                    consumer.Commit(consumeResult);
+            //                }
+            //                catch (KafkaException e)
+            //                {
+            //                    Console.WriteLine($"Commit error: {e.Error.Reason}");
+            //                }
+            //            }
+            //            catch (ConsumeException e)
+            //            {
+            //                Console.WriteLine($"Consume error: {e.Error.Reason}");
+            //            }
+            //        }
+            //    }
+            //    catch (OperationCanceledException)
+            //    {
+            //        Console.WriteLine("Closing consumer.");
+            //        consumer.Close();
+            //    }
+            //}
+
+            //.SetValueDeserializer(new AvroDeserializer<GenericRecord>())
+            using (var consumer = new ConsumerBuilder<int, GenericRecord>(config).SetValueDeserializer(new AvroDeserializer<GenericRecord>(schemaRegistry).AsSyncOverAsync()).Build())
+            //using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+
+                consumer.Subscribe(topicName);
+                //var partion = new TopicPartition(topicName, new Partition(0));
+                //consumer.Assign(partion);
+                //consumer.Seek(new TopicPartitionOffset(partion, 0));
+                int total = 0;
+
+                while (true)
+                {
+                    
+                    var cr = consumer.Consume();
+
+
+
+                    //try to get real model
+                    var valueString = cr.Message.Value.ToString().Split("contents:")[1];
+
+                    var model = JsonConvert.DeserializeObject<TestModel>(valueString);// JsonSerializer.d
+
+                    Console.WriteLine(cr.Message.Key + "----" + model.ToString());
+                }
+            }
+                Console.Read();
+
+
+        }
+    }
+}
---
+++ Consumer_Test/price_model.cs
@@ -0,0 +1,100 @@
+﻿using Avro;
+using Avro.Specific;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.confluent.ksql.avro_schemas
+{
+    public class TestModel
+    {
+        public decimal UNITPRICE { get; set; }
+        public decimal totalMoney { get; set; }
+
+        public decimal totalCount { get; set; }
+
+        public override string ToString()
+        {
+			return $"TotalMoney : {totalMoney}, totalcount : {totalCount}, unitprice: {UNITPRICE}";
+        }
+    }
+
+	public class KsqlDataSourceSchema : ISpecificRecord
+	{
+		//public static Schema _SCHEMA = Schema.Parse("{\"type\":\"record\",\"name\":\"Item\",\"namespace\":\"MyTestDemo.Model\",\"fields\":[{\"name\":\"Id\",\"type\":\"int\"},{\"name\":\"Name\",\"type\":\"string\"},{\"name\":\"ItemCount\",\"type" +
+		//"\":\"float\"},{\"name\":\"TotalMoney\",\"type\":\"float\"}]}");
+
+		//public static Schema _SCHEMA = Schema.Parse("{\"type\":\"record\",\"name\":\"Value\",\"namespace\":\"ca6340e403ac.dbo.Items\",\"fields\":[{\"name\":\"Id\",\"type\":\"int\"},{\"name\":\"Name\",\"type\":\"string\"},{\"name\":\"ItemCount\",\"type\":\"double\"},{\"name\":\"TotalMoney\",\"type\":\"double\"},{\"name\":\"__deleted\",\"default\":null,\"type\":[\"null\",\"string\"]}]}");
+
+		//public static Schema _SCHEMA = Schema.Parse("{\"type\":\"record\",\"name\":\"KsqlDataSourceSchema\",\"namespace\":\"io.confluent.ksql.avro_schemas\",\"fields\":[{\"name\":\"TOTALMONEY\",\"default\":null,\"type\":[\"null\",\"bytes\"]},{\"name\":\"TOTALCOUNT\",\"default\":null,\"type\":[\"null\",\"int\"]},{\"name\":\"UNITPRICE\",\"default\":null,\"type\":[\"null\",\"bytes\"]}]}");
+
+		public static Schema _SCHEMA = Schema.Parse("{\"type\":\"record\",\"name\":\"KsqlDataSourceSchema\",\"namespace\":\"io.confluent.ksql.avro_schemas\",\"fields\":[{\"name\":\"TOTALMONEY\",\"type\":\"bytes\"},{\"name\":\"TOTALCOUNT\",\"type\":[\"null\",\"int\"]},{\"name\":\"UNITPRICE\",\"type\":[\"null\",\"bytes\"]}]}");
+
+		public virtual Schema Schema
+		{
+			get
+			{
+				return KsqlDataSourceSchema._SCHEMA;
+			}
+		}
+		public int TOTALCOUNT { get; set; }
+		public decimal TOTALMONEY { get; set; }
+
+		public byte[] UNITPRICE { get; set; }
+
+		public object Get(int fieldPos)
+		{
+			switch (fieldPos)
+			{
+				case 0: return this.TOTALMONEY;
+				case 1: return this.TOTALCOUNT;
+				case 2: return this.UNITPRICE;
+				default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Get()");
+			};
+		}
+
+		public void Put(int fieldPos, object fieldValue)
+		{
+			switch (fieldPos)
+			{
+				case 0:
+					var newval = (byte[])fieldValue;
+					var result = System.Text.Encoding.Default.GetChars(newval);
+					
+
+					string output = string.Join(" ", result.Select(c => (uint)c));
+		
+
+					this.TOTALMONEY = Convert.ToDecimal(output); break;
+				case 1: this.TOTALCOUNT = (System.Int32)fieldValue; break;
+				case 2:
+
+					this.UNITPRICE = (byte[])fieldValue; break;
+
+				default: throw new AvroRuntimeException("Bad index " + fieldPos + " in Put()");
+			};
+		}
+
+		public static double ByteArrayToDouble(byte[] src)
+		{
+			using (MemoryStream stream = new MemoryStream(src))
+			{
+				stream.Position = 0;
+				// Create a BinaryReader to read the decimal from the stream
+				using (BinaryReader reader = new BinaryReader(stream))
+				{
+					// Read and return the decimal from the
+					// BinaryReader/MemoryStream
+					var aa = reader.ReadInt32();
+					var ss = reader.ReadDecimal();
+
+					return 0.0;
+				}
+			}
+
+		}
+	}
+}
